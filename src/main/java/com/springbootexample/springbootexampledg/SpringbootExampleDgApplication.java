e93be585e5e8a8380d490edaaa3577910b5ffd6d
mukkamukka <rlaehdrjs4605@gmail.com>
2023-04-25 14:24:32 +0900

    Springboot project initial configuration

---
+++ src/main/java/com/springbootexample/springbootexampledg/SpringbootExampleDgApplication.java
@@ -0,0 +1,13 @@
+package com.springbootexample.springbootexampledg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootExampleDgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootExampleDgApplication.class, args);
+	}
+
+}
